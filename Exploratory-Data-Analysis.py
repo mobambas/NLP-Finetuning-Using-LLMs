e189c87a997603024cfb3726e10832288471e739
Shriyansh Singh <99650602+mobambas@users.noreply.github.com>
2024-05-11 12:41:25 +0800

    Create Exploratory-Data-Analysis.py

---
+++ Exploratory-Data-Analysis.py
@@ -0,0 +1,3 @@
+##Q1) Plot the class distribution for the chosen dataset(s). If you are working with different datasets, compare the distributions. Attach your plots and code snippets.
+
+#First, we need to download the reddit_tifu dataset (since the dataset is in the croissant format, it is available to download in the Hugging Face model hub). If we don't have the datasets library installed, install it using the following command in your terminal.
